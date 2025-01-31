914687db2b0ca6a896d66af0578f565ad5f4cf30
Velagasseth <sethvelagas.ke@gmail.com>
2025-01-31 12:40:26 +0300

    Add files via upload

---
+++ 2_Resume.py
@@ -0,0 +1,55 @@
+import streamlit as st
+st.title(":red[Resume]")
+col1, col2=st.columns(2)
+with col1:
+    st.write("""
+SETH OPIYO ODERO,
+\nKisumu county, kenya,
+\nEmail : velagasseth@gmail.com
+\nTell : +254759812041
+ \nGitHub : https://github.com/Velagasseth
+\nLinkedin : www.linkedin.com/in/velagasseth-tech
+""")
+
+
+data=st.write("""
+
+#### ***Objective***\n
+Passionate data  science student with a strong foundation in statistics, machine learning and data visualization seeking internship or entry level role where I can apply my analytic skills and contribute to solving complex problem using data .\n
+A highly motivated and detail-oriented machine learning student at Kisii University with strong skills in data manipulation, data visualization, and web scraping. Seeking an internship or entry-level position to apply my knowledge and skills in a practical environment.
+
+
+#### ***Education***\n
+Kisii University, Kisii County, Kenya Bachelor of Applied Science in Computer Science Expected Graduation: December 2027
+
+ #### ***Skills***\n
+:blue[Data Manipulation:]
+Proficient in using Pandas for cleaning, transforming, and analyzing data.\n
+:blue[Data Visualization:]
+Experienced in creating insightful visualizations using matplotlib and seaborn plotting capabilities\n
+:blue[Web Scraping:]
+Skilled in using BeautifulSoup for extracting data from websites\n
+:blue[Programming Languages:]
+Python, SQL, \n
+:blue[Tools and Libraries:]
+Jupyter Notebook, NumPy, Matplotlib, Seaborn, pycharm\n
+Projects
+#### ***Projects***\n   
+Data Analysis of Retail Sales\n
+Collected and cleaned data using Pandas.\n
+Performed exploratory data analysis to identify sales trends.\n
+Visualized sales data using Matplotlib and Seaborn to present findings.\n
+Web Scraping for Market Research\n
+Developed a web scraper using BeautifulSoup to extract product information from e-commerce websites.\n
+Analyzed and visualized the extracted data to provide insights into market trends.\n
+#### ***Extracurricular Activities***\n 
+Machine Learning Club, Kisii University
+Organized workshops and hackathons on data science and machine learning.\n
+Collaborated with peers on various machine learning projects.
+Volunteer, Kisii County Community Center\n
+Conducted coding sessions for high school students to inspire interest in technology.
+
+
+         """)
+
+         
