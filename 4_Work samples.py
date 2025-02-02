1b147d46ee502d09070916bd60036c8ace41e2e3
Velagasseth <sethvelagas.ke@gmail.com>
2025-02-02 12:09:00 +0300

    Add files via upload

--- 4_Work samples.py
+++ 4_Work samples.py
@@ -5,3 +5,9 @@ col1, col2=st.columns(2)
 with col1:
     st.subheader(":red[Data/Projects]")
     st.write("Detailed case studies demonstrating my approach to real-world data challenges, showcasing techniques such as :blue[regression analysis, clustering, and deep learning using pytorch]")
+    st.write("""#### Using Scikit-learn datasets ;\n
+    >Breast_Cancer_Dataset\n
+    >Iris_Datasets\n
+    >Wine_Dataset\n
+    """)
+    st.write("Link to the project [https://myportfolio-iojbai9uggilvkrwjjmvvz.streamlit.app/](https://myportfolio-iojbai9uggilvkrwjjmvvz.streamlit.app/)""")
